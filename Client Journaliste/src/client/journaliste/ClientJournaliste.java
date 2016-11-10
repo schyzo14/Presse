ea4afb65228d06557bd53efc69e883f61e111b61
Manon <manon.fabarez@gmail.com>
2016-11-10 15:34:35 +0100

    Test du commit

--- Client Journaliste/src/client/journaliste/ClientJournaliste.java
+++ Client Journaliste/src/client/journaliste/ClientJournaliste.java
@@ -16,6 +16,7 @@ public class ClientJournaliste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Test de push");
     }
     
 }
