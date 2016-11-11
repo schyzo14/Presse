d47163be276d53752045b068ee3ffda05da26fb8
Aurore <aurore.queille@free.fr>
2016-11-11 17:54:12 +0100

    Menus distributeur, Creer Compte

--- ClientDistributeur/src/client/distributeur/Vue/Menu/MenuAvantConnexion.java
+++ ClientDistributeur/src/client/distributeur/Vue/Menu/MenuAvantConnexion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package client.distributeur.Vue;
+package client.distributeur.Vue.Menu;
 
 /**
  *
