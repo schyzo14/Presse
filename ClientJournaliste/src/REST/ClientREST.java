68fc29e762f1b92765b9b544cf5e4a745b51adc9
Manon <manon.fabarez@gmail.com>
2016-11-15 13:57:25 +0100

    Client REST Journaliste

---
+++ ClientJournaliste/src/REST/ClientREST.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REST;
+
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ * Jersey REST client generated for REST resource:articleRessource
+ * [articles]<br>
+ * USAGE:
+ * <pre>
+ *        ClientREST client = new ClientREST();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author manou
+ */
+public class ClientREST {
+
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/TransmissionArticles-war/webresources";
+
+    public ClientREST() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("articles");
+    }
+
+    public String postJson() throws ClientErrorException {
+        return webTarget.request().post(null, String.class);
+    }
+
+    public <T> T getJson(Class<T> responseType) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public void close() {
+        client.close();
+    }
+    
+}
