b2189502131a3674996aabf530458d5beaaff630
Manon <manon.fabarez@gmail.com>
2016-11-18 08:58:28 +0100

    ClientJournaliste - MiseàJour REST

--- ClientJournaliste/src/REST/ClientREST_MotsClesJournaliste.java
+++ ClientJournaliste/src/REST/ClientREST_MotsClesJournaliste.java
@@ -12,11 +12,11 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Jersey REST client generated for REST resource:articleRessource
- * [articles]<br>
+ * Jersey REST client generated for REST resource:motsclesRessource
+ * [motscles]<br>
  * USAGE:
  * <pre>
- *        ClientREST client = new ClientREST();
+ *        ClientREST_MotsClesJournaliste client = new ClientREST_MotsClesJournaliste();
  *        Object response = client.XXX(...);
  *        // do whatever with response
  *        client.close();
@@ -24,24 +24,25 @@ import javax.ws.rs.core.MediaType;
  *
  * @author manou
  */
-public class ClientREST {
+public class ClientREST_MotsClesJournaliste {
 
     private WebTarget webTarget;
     private Client client;
     private static final String BASE_URI = "http://localhost:8080/TransmissionArticles-war/webresources";
 
-    public ClientREST() {
+    public ClientREST_MotsClesJournaliste() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
-        webTarget = client.target(BASE_URI).path("articles");
+        webTarget = client.target(BASE_URI).path("motscles");
     }
 
-    public String postJsonJournaliste(String nomArticle, String nomAuteur, String contenu, String motscles) throws ClientErrorException {
+    public String postJsonMotsClesJournaliste(String nomArticle, String nomAuteur, String contenu, String motscles) throws ClientErrorException {
         webTarget = webTarget.queryParam("nomArticle", nomArticle);
         webTarget = webTarget.queryParam("nomAuteur", nomAuteur);
         webTarget = webTarget.queryParam("contenu", contenu);
         webTarget = webTarget.queryParam("motscles", motscles);
         
         return webTarget.request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(MediaType.APPLICATION_JSON), String.class);
+    
     }
 
     public String getJson() throws ClientErrorException {
