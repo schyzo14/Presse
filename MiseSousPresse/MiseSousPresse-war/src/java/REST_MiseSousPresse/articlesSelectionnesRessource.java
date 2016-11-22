b17de844f535eb0523c1a410d3e24c5a09a8c2ea
schyzo14 <youssef.darrab@gmail.com>
2016-11-22 21:37:37 +0100

    Récupération des articlesSelectionnes uniquement

---
+++ MiseSousPresse/MiseSousPresse-war/src/java/REST_MiseSousPresse/articlesSelectionnesRessource.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package REST_MiseSousPresse;
+
+import REST.PresseBeanLocal;
+import com.google.gson.Gson;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ *
+ * @author Schyzo
+ */
+@Path("articlesSel")
+public class articlesSelectionnesRessource {
+    //Accès backoffice
+    PresseBeanLocal presseBean;
+    
+    @Context
+    private UriInfo context;
+    
+    //Convertisseur json
+    private final Gson gson;
+    
+    // Constructeur de la ressource
+    public articlesSelectionnesRessource() {
+        this.gson = new Gson();
+        this.presseBean = lookupPresseBeanLocal();
+    }
+    
+    //Récupérer la liste des articles
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getListeArticlesSelectionnes() {
+        return this.gson.toJson(this.presseBean.listeArticleSelect());
+    }
+    
+    /**
+     * Recherche JNDI BackOffice
+     *
+     * @return la référence vers le BackOffice
+     */
+    private PresseBeanLocal lookupPresseBeanLocal() {
+        try {
+            javax.naming.Context c = new InitialContext();
+            return (PresseBeanLocal) c.lookup("java:global/MiseSousPresse/MiseSousPresse-ejb/PresseBean!REST.PresseBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+}
