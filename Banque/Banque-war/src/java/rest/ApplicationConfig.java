b67e98a3458ceb1cf790781a07e050047f2ee277
Aurore <aurore.queille@free.fr>
2016-11-23 14:01:58 +0100

    Banque-war -> ok

--- Banque/Banque-war/src/java/rest/ApplicationConfig.java
+++ Banque/Banque-war/src/java/rest/ApplicationConfig.java
@@ -1,24 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
 /**
- *
  * ApplicationConfig
- *
- * @author Cédric Teyssié  <cedric.teyssie@irit.fr>, IRIT-SIERA, Université Paul Sabatier
- * @version 0.1, 5 oct. 2016
- * @since 0.1, 5 oct. 2016
  */
-// BanqueREST
-// org.miage.jee.eai.banque.expo.rest
-// ApplicationConfig.java
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
 
@@ -29,12 +16,8 @@ public class ApplicationConfig extends Application {
         return resources;
     }
 
-    /**
-     * Do not modify addRestResourceClasses() method. It is automatically populated with all resources defined in the project. If required,
-     * comment out calling this method in getClasses().
-     */
+
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(rest.CompteResource.class);
- }
-
+    }
 }
--- Banque/Banque-war/src/java/rest/CompteResource.java
+++ Banque/Banque-war/src/java/rest/CompteResource.java
@@ -15,13 +15,15 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import services.BanqueBeanLocal;
-//import entities.Payement;
 
+/**
+ * CompteRessources : compte avec payements
+ */
 @Path("comptes/{numCompte}")
 public class CompteResource {
 
+    // BanqueBeanLocal
     BanqueBeanLocal banqueBean1 = lookupBanqueBeanLocal();
-
     BanqueBeanLocal banqueBean;
 
     @Context
@@ -29,6 +31,9 @@ public class CompteResource {
 
     private Gson gson;
 
+    /**
+     * Constructeur
+     */
     public CompteResource() {
         this.banqueBean = lookupBanqueBeanLocal();
         this.gson = new Gson();
@@ -37,11 +42,11 @@ public class CompteResource {
     /**
      * http://localhost:8080/Banque-war/webresources/comptes/10001111/?nomPayeur=Hachette Diffusion&numCompteReception=11110000&montant=1000
      *
-     * @param numCompte
-     * @param nomPayeur
-     * @param numCompteReception
-     * @param montant
-     * @return 
+     * @param numCompte         numéro du débiteur
+     * @param nomPayeur         nom du débiteur
+     * @param numCompteReception numéro du créditeur
+     * @param montant           montant du payement
+     * @return                  le json du payement
      */    
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -49,21 +54,31 @@ public class CompteResource {
             @PathParam("numCompte") String numCompte, 
             @QueryParam("nomPayeur") String nomPayeur, @QueryParam("numCompteReception") String numCompteReception, @QueryParam("montant") String montant) {
         
+        // numéro du débiteur
         int numComptePayeur = Integer.parseInt(numCompte);
+        // numéro du créditeur
         int numCompteReceptionInt = Integer.parseInt(numCompteReception);
+        // montant du payement
         double montantD = Double.parseDouble(montant);
+        
         Payement payement;
         
         try {
+            // réaliser le payement dans BanqueBean
             payement = this.banqueBean.payer(nomPayeur, numComptePayeur, numCompteReceptionInt, montantD);
-            
         } catch (Throwable e) {
+            // Erreur lors du payement
             return this.gson.toJson(e);
         }
 
+        // renvoyer le payement en json
         return this.gson.toJson(payement);
     }
 
+    /**
+     * lookupBanqueBeanLocal
+     * @return BanqueBeanLocal
+     */
     private BanqueBeanLocal lookupBanqueBeanLocal() {
         try {
             javax.naming.Context c = new InitialContext();
@@ -73,6 +88,4 @@ public class CompteResource {
             throw new RuntimeException(ne);
         }
     }
-
-
 }
