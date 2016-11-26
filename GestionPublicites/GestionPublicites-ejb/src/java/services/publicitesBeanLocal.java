9ce3279163c6716a98c3f2dd29c9ac784687c149
schyzo14 <youssef.darrab@gmail.com>
2016-11-26 12:00:49 +0100

    Ajout des commentaires

--- GestionPublicites/GestionPublicites-ejb/src/java/services/publicitesBeanLocal.java
+++ GestionPublicites/GestionPublicites-ejb/src/java/services/publicitesBeanLocal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package services;
 
 import java.util.HashMap;
@@ -10,8 +5,8 @@ import javax.ejb.Local;
 import presse.publicite;
 
 /**
- *
- * @author Schyzo
+ * Interface du serveur de publicités
+ * Implémentée par publicitesBean
  */
 @Local
 public interface publicitesBeanLocal {
